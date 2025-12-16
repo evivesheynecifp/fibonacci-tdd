be3053b23a3337f7d2e663eebceb485e60da41e4
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-16 21:06:59 +0100

    Pass test 2

--- fibonacci-tdd/src/main/java/daw/dac/Fibonacci.java
+++ fibonacci-tdd/src/main/java/daw/dac/Fibonacci.java
@@ -5,6 +5,8 @@ public class Fibonacci {
     public int calculate(int n) {
         if (n == 1) {
             return 1;
+        } else if (n == 2) {
+            return 1;
         }
         return 0;
     }
