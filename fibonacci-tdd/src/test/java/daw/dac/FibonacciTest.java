cc1add43dd830ee7cb983fdff211ca803f03d190
evivesheyne <evivesheyne@cifpfbmoll.eu>
2025-12-21 12:54:45 +0100

    Add new validation tests

--- fibonacci-tdd/src/test/java/daw/dac/FibonacciTest.java
+++ fibonacci-tdd/src/test/java/daw/dac/FibonacciTest.java
@@ -1,12 +1,17 @@
 package daw.dac;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class FibonacciTest {
 
     private Fibonacci fibonacci = new Fibonacci();
 
+    // Old Tests
     @Test
     void fibonacciOfZeroIsZero() {
         assertEquals(0, fibonacci.calculate(0));
@@ -50,5 +55,17 @@ class FibonacciTest {
     @Test
     void fibonacciOfEightIsTwentyOne() {
         assertEquals(21, fibonacci.calculate(8));
+    }
+
+    // New Tests
+    @Test
+    void fibonacciOfNegativeThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> fibonacci.calculate(-1));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 5, 10, 20})
+    void fibonacciAlwaysReturnsNonNegativeValue(int n) {
+        assertTrue(fibonacci.calculate(n) >= 0);
     }
 }
